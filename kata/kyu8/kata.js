948f5f3452be13c6be3444bccf89e0a3f3056c58
Vlad Radzivil <vladradziviloff@gmail.com>
2022-08-21 12:16:05 +0200

    add 16 honor, point total:88

--- kata/kyu8/kata.js
+++ kata/kyu8/kata.js
@@ -75,7 +75,7 @@ Your task is to write a function which returns the time since midnight in millis
 
 // console.log(past(1,1,1));
 
-///////////////////////////// 5 ////////////////////////
+///////////////////////////// 6 ////////////////////////
 /*
 Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 */
@@ -88,3 +88,103 @@ Given a set of numbers, return the additive inverse of each. Each positive becom
 //  }
 // console.log(invert([1,-2,3,-4,5]));
 // console.log(invert([0]));
+
+///////////////////////////// 7 ////////////////////////
+
+/*
+In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+If you can, try writing it in only one line of code.
+ */
+
+// function findMultiples(integer, limit) {
+//     let x = [];
+//     x.push(integer);
+//     let y = integer;
+//     while(x.length < Math.floor(limit/integer)){
+//         x.push(y = y + integer);
+//     }
+//     return x;
+//   }
+
+
+// console.log(findMultiples(5, 25));
+// console.log(findMultiples(1, 2));
+// console.log(findMultiples(5, 7));
+// console.log(findMultiples(4, 27));
+// console.log(findMultiples(11, 54));
+
+// console.log(27 / 4);
+// console.log(Math.floor(27/4));
+
+///////////////////////////// 8 ////////////////////////
+/*Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).*/
+
+// function powersOfTwo(n){
+//     let x = [1,];
+//     for (let i = 1; i <= n; i++) {
+//         x.push(2 ** i);
+//     }
+//     return x;
+//   }
+
+// console.log(powersOfTwo(0));
+// console.log(powersOfTwo(1));
+// console.log(powersOfTwo(2));
+// console.log(powersOfTwo(4));
+
+///////////////////////////// 9 ////////////////////////
+// Build a function that returns an array of integers from n to 1 where n>0.
+// Example : n=5 --> [5,4,3,2,1]
+
+// const reverseSeq = n => {
+//     let x = [];
+//     for (let i = n; i > 0; i--) {
+//         x.push(i);
+//     }
+//     return x;
+//   };
+
+// console.log(reverseSeq(5));
+///////////////////////////// 10 ////////////////////////
+/*Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.*/
+// function lovefunc(flower1, flower2){
+//     return flower1 % 2 !== flower2 % 2;
+//   }
+
+// console.log(lovefunc(2, 2));
+// console.log(lovefunc(2, 1));
+// console.log(lovefunc(143, 353));
+
+
+
+///////////////////////////// 11 ////////////////////////
+// function removeChar(str){
+
+//     return str.slice(1,-1);
+  
+// };
+
+// console.log(removeChar('eloquent'));
+
+///////////////////////////// 12 ////////////////////////
+// function isPythagoreanTriple(integers) {
+//     let c = integers[2] ** 2;
+//     let b = integers[1] ** 2;
+//     let a = integers[0] ** 2;
+
+//     if(a+b >=c) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(isPythagoreanTriple([3, 4, 5]));
+// console.log(isPythagoreanTriple([70, 18, 8]));
+// console.log(isPythagoreanTriple([3, 5, 9]));
+// console.log(isPythagoreanTriple([72, 78, 30]));
